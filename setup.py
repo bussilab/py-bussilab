fed138977952af401a4524706cf1732cb871edb8
Giovanni <giovanni.bussi@gmail.com>
2020-07-10 09:22:25 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -24,18 +24,20 @@ def description():
 setup(
     name="bussilab",
     version=version(),
+    author='Giovanni Bussi',
+    author_email='bussi@sissa.it',
     description=description(),
     long_description=readme(),
     long_description_content_type='text/markdown',
-    author='Giovanni Bussi',
-    author_email='bussi@sissa.it',
+    url='https://github.com/bussilab/py-bussilab',
+    packages=['bussilab'],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Programming Language :: Python :: 3 :: Only",
         "Topic :: Scientific/Engineering"
-        ],
-    packages=['bussilab'],
+        ],    
     install_requires=deps(),
     python_requires='>=3.6',
     scripts=['bin/bussilab']
