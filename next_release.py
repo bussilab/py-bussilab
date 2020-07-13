c2b96aaf8dd8bab038ab42568a0c8cb6112c4651
Giovanni Bussi <giovanni.bussi@gmail.com>
2020-07-13 09:26:08 +0200

    script for new releases

---
+++ next_release.py
@@ -0,0 +1,52 @@
+import re
+import os
+from bussilab import __version__
+
+def confirm():
+    cont=True
+    while cont:
+        response=input("Confirm (yes/no)?")
+        if re.match("[Yy][Ee][Ss]",response) :
+            cont=False
+            response='yes'
+        elif re.match("[Nn][Oo]",response) :
+            quit()
+        else:
+            pass
+
+print("Current version:",__version__)
+new_version=re.sub("[0-9]*$","",__version__) + str(int(re.sub("^.*\.","",__version__))+1)
+
+response=input("New version (default " + new_version + "):")
+
+if len(response)>0:
+    new_version=response
+
+print("New version "+new_version)
+
+confirm()
+
+lines=[]
+with open("bussilab/_version.py") as f:
+  for line in f:
+      line=re.sub("^ *__version__ *=.*$",'__version__ = "' + new_version + '"',line)
+      lines.append(line)
+
+with open("bussilab/_version.py","w") as f:
+    for line in lines:
+       print(line,file=f,end='')
+cmd=[
+  'git commit --allow-empty -m "Version ' + new_version + '"',
+  'git tag v' + new_version,
+  'git push origin master v' + new_version
+  ]
+
+print("Will now execute the following commands:")
+for c in cmd:
+    print("  " + c)
+
+confirm()
+
+for c in cmd:
+   os.system(c)
+
