a84941efc05cc5a33f031defd13161560c05ee9e
Giovanni Bussi <giovanni.bussi@gmail.com>
2020-07-14 15:06:06 +0200

    faster cron test

--- tests/test_cron.py
+++ tests/test_cron.py
@@ -13,20 +13,27 @@ class TestCron(TestCase):
                 os.remove("cron.out")
             except FileNotFoundError:
                 pass
-            cli("cron --cron-file cron.yml --period 2 --max-times 2 --no-screen")
-            self.assertEqualFile("cron.out")
-            os.remove("cron.out")
-
-    def  test_cron_screen(self):
-        with cd(os.path.dirname(os.path.abspath(__file__))):
             try:
                 os.remove("cron_screen.out")
             except FileNotFoundError:
                 pass
+            try:
+                os.remove("cron_screen2.out")
+            except FileNotFoundError:
+                pass
+            # this is asynchronous
             cli("cron --cron-file cron_screen.yml --period 2 --max-times 2 --detach")
-            time.sleep(6)
+            now=time.time()
+            # this is synchronous, and will take some time
+            cli("cron --cron-file cron.yml --period 2 --max-times 2 --no-screen")
+            # make sure to give enough time to the asynchronous test
+            time.sleep(6-(time.time()-now))
+            self.assertEqualFile("cron.out")
             self.assertEqualFile("cron_screen.out")
+            self.assertEqualFile("cron_screen2.out")
+            os.remove("cron.out")
             os.remove("cron_screen.out")
+            os.remove("cron_screen2.out")
 
 if __name__ == "__main__":
     unittest.main()
