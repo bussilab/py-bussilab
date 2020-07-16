f5905f6550c56409c7cce61cb8a5b64213698759
Giovanni Bussi <giovanni.bussi@gmail.com>
2020-07-16 17:13:57 +0200

    description

--- make_conda_recipe.py
+++ make_conda_recipe.py
@@ -3,6 +3,10 @@ import re
 from bussilab import required_conda
 from bussilab import __version__
 
+def description():
+    from bussilab import __doc__ as doc
+    return doc.partition('\n')[0]
+
 with open('conda/meta.yaml.in') as f:
     recipe=f.read()
 
@@ -16,6 +20,9 @@ for r in ast.literal_eval(required_conda()):
     requirements+=match.group(1)+"- " + r+"\n"
 
 recipe=re.sub("( *)(__REQUIRED__)\n",requirements,recipe)
+
+recipe=re.sub("__SUMMARY__",description(),recipe)
+recipe=re.sub("__DESCRIPTION__",description(),recipe)
 
 with open('conda/meta.yaml',"w") as f:
     f.write(recipe)
