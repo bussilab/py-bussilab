ecc2c68af88402cf03674f983e90238d2c42c714
Giovanni Bussi <giovanni.bussi@gmail.com>
2020-07-22 09:45:20 +0200

    test

---
+++ tests/test_clustering.py
@@ -0,0 +1,32 @@
+import unittest
+
+import numpy as np
+import scipy.spatial.distance as distance
+from bussilab.coretools import TestCase
+
+
+class TestClustering(TestCase):
+    def test_maxclique(self):
+        from bussilab.clustering import max_clique
+        data=(np.array(range(50))**2).reshape(-1,1)
+        dist=distance.squareform(distance.pdist(data))
+        adj=np.int_(dist<1000)
+        cl=max_clique(adj)
+        self.assertEqual(cl.method,"max_clique")
+        self.assertEqual(cl.weights,[32, 13, 5])
+        ref=[[31, 30, 28, 29, 27, 25, 26, 23, 24, 22, 20, 21, 18, 19, 16, 17, 13, 14, 15, 10, 11, 12, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4], [38, 39, 40, 41, 42, 43, 44, 37, 35, 36, 34, 33, 32], [45, 48, 49, 46, 47]]
+        # compare sets, since order is irrelevant
+        for i in range(len(cl.clusters)):
+            self.assertEqual(set(ref[i]),set(cl.clusters[i]))
+
+        weights=np.array(range(50))+1
+        cl=max_clique(adj,weights)
+        self.assertEqual(cl.weights,[546, 546, 99, 69, 15])
+        ref=[[45, 38, 39, 40, 41, 42, 43, 44, 37, 35, 36, 47, 46], [13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 10, 11, 12, 5, 6, 7, 8, 9, 32], [48, 49], [33, 34], [0, 1, 2, 3, 4]]
+        for i in range(len(cl.clusters)):
+            self.assertEqual(set(ref[i]),set(cl.clusters[i]))
+
+
+if __name__ == "__main__":
+    unittest.main()
+
