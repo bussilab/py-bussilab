a15379aa46e5804cc6b59395e38cae7ddd2e8044
Giovanni Bussi <giovanni.bussi@gmail.com>
2020-08-24 12:55:49 +0200

    allow testing notify

---
+++ tests/test_notify.py
@@ -0,0 +1,28 @@
+import os
+import unittest
+
+from bussilab.notify import notify
+
+# only run tests if env vars are configured
+if 'BUSSILAB_TEST_NOTIFY_TOKEN' in os.environ:
+    token=os.environ["BUSSILAB_TEST_NOTIFY_TOKEN"]
+    channel=os.environ["BUSSILAB_TEST_NOTIFY_CHANNEL"]
+    class TestNotify(unittest.TestCase):
+        def test_notify(self):
+            notify("unittest1", token=token, channel=channel)
+
+            url=notify("unittest2 *WRONG*", token=token, channel=channel)
+            notify("unittest2", update=url, token=token)
+
+            url=notify("unittest3 *WRONG*", token=token, channel=channel)
+            url=notify("unittest3 *WRONG 2*", update=url, token=token)
+            notify(delete=url, token=token)
+
+            with self.assertRaises(Exception):
+                notify("unittest3 *WRONG 3*", update=url, token=token)
+
+            with self.assertRaises(Exception):
+                notify(delete=url, token=token)
+
+if __name__ == "__main__":
+    unittest.main()
